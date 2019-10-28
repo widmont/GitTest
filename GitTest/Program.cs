8262f30de906f68172c5d5b32c6779d58364910f
antoine.widmer <antoine.widmer@hevs.ch>
2019-10-28 08:51:05 +0100

    added a new console line

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,6 +7,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("this is my first commit");
         }
     }
 }
